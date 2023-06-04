826150e0b7c39c3bd68d15c99bc1765af8d1e4bc
Luis Melendez Lenovo <luis_melendez_lenovo@gmail.com>
2023-06-04 12:25:41 -0600

    Move initialization and rendering to functions and create game loop.

--- SDLGame/src/main.cpp
+++ SDLGame/src/main.cpp
@@ -1,30 +1,57 @@
 #include<SDL.h>
+#include <iostream>
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
+bool g_bRunning = false;
 
-int main(int argc, char* args[])
+bool init(const char* title, int xpos, int ypos, int	height, int width, int flags)
 {
-
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
-		g_pWindow = SDL_CreateWindow("Chapter 1: Setting up SDL", SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-
+		g_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
 		if (g_pWindow != 0)
 		{
 			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
 		}
+		else
+		{
+			return false;
+		}
 	}
 	else
 	{
-		return 1;
+		return false;
 	}
+	return true;
+}
 
+void render()
+{
 	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
 	SDL_RenderClear(g_pRenderer);
 	SDL_RenderPresent(g_pRenderer);
-	SDL_Delay(5000);
+}
+
+int main(int argc, char* args[])
+{
+	if (init("Chapter 1: Setting up SDL", SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN))
+	{
+		g_bRunning = true;
+		std::cout << "SDL initialized and Window created!" << std::endl;
+	}
+	else
+	{
+		g_bRunning = false;
+		std::cout << "SDL not initialized or Window not created!" << std::endl;
+	}
+
+	while (g_bRunning)
+	{
+		render();
+	}
+
 	SDL_Quit();
 
 	return 0;
